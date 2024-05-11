15509023e4d6c3fff77135b2fbf9ac45e27de187
SLValerio <vsirlance@gmail.com>
2024-05-11 16:35:29 +0800

    add reactive form trial files

---
+++ 6. may-11-2024/trials/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, 
+  AbstractControl, ValidatorFn } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email, this.failIfHasTest,
+        this.failIfHasWord('bad', 'bad_word', 'Contain bad word!')
+      ]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (!this.loginForm.valid) return;
+
+    console.log(this.loginForm.value);
+  }
+
+  get emailControl() {
+    return this.loginForm.get('email')!;
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password')!;
+  }
+
+  failIfHasTest(control: AbstractControl) {
+    if (!control.value.toLowerCase().includes('test')) {
+      return null;
+    }
+    return { test_data: 'value is invalid' };
+  }
+
+  failIfHasWord(word: string, errCode: string, errMsg: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      if (!control.value.toLowerCase().includes(word)) {
+        return null;
+      }
+      return { [errCode]: errMsg };
+    }
+
+  }
+
+}
