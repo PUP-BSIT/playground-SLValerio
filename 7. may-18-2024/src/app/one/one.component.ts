dc8046b7f1461ab35405c4ab3dfed5c4120051bc
SLValerio <vsirlance@gmail.com>
2024-05-20 20:20:10 +0800

    add service/dependecy trials

---
+++ 7. may-18-2024/src/app/one/one.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from '../../service/counter.service';
+
+@Component({
+  selector: 'app-one',
+  styleUrls: ['./one.component.css'],
+  template: `
+    <button (click)="increment()">Increment from Component 1</button>
+  `,
+})
+export class OneComponent implements OnInit {
+  constructor(private counterService: CounterService) {}
+
+  increment() {
+    this.counterService.increment();
+    console.log(`Comp 1 - count: ${this.counterService.getCount()}`);
+  }
+
+  ngOnInit(): void {
+    this.counterService.onUpdate.subscribe((newValue) => {
+      console.log(`Comp 1 - count is updated: ${newValue}`);
+    });
+  }
+}
---
+++ 7. may-18-2024/src/app/two/two.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from '../../service/counter.service';
+
+@Component({
+  selector: 'app-two',
+  styleUrls: ['./two.component.css'],
+  template: `
+    <button (click)="increment()">Increment from Component 2</button>
+  `,
+})
+export class TwoComponent implements OnInit {
+  constructor(private counterService: CounterService) {}
+
+  increment() {
+    this.counterService.increment();
+    console.log(`Comp 2 - count: ${this.counterService.getCount()}`);
+  }
+
+  ngOnInit(): void {
+    this.counterService.onUpdate.subscribe((newValue) => {
+      console.log(`Comp 2 - count is updated: ${newValue}`);
+    });
+  }
+}
